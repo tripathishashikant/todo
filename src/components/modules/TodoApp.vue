ec5b378db98851e8ecf7e82dbb33756274294076
Shashikant <shashikant.tripathi187@gmail.com>
2022-11-25 00:59:37 +0530

    Logic updation after modifying list object to support multiple list.

--- src/components/modules/TodoApp.vue
+++ src/components/modules/TodoApp.vue
@@ -8,12 +8,7 @@
         <add-task @addNewTask="addTask"></add-task>
       </base-card>
       <base-card>
-        <pending-tasks :todos="todos" @markComplete="toggleCompleted">
-        </pending-tasks>
-      </base-card>
-      <base-card>
-        <completed-tasks :todos="todos" @markPending="toggleCompleted">
-        </completed-tasks>
+        <todo-list :lists="lists" @markComplete="toggleCompleted"></todo-list>
       </base-card>
     </main>
   </section>
@@ -21,43 +16,49 @@
 
 <script>
 import AppHeader from './AppHeader.vue';
-import PendingTasks from './PendingTasks.vue';
-import CompletedTasks from './CompletedTasks.vue';
+import TodoList from './TodoList.vue';
 import AddTask from './AddTask.vue';
 
 export default {
   name: 'todoApp',
   components: {
     AppHeader,
-    PendingTasks,
-    CompletedTasks,
+    TodoList,
     AddTask,
   },
   data() {
     return {
       title: 'Todo App',
-      todos: [
+      lists: [
         {
+          listName: 'List 1',
           id: 1,
-          title: 'Learn Vue',
-          completed: false,
-        },
-        {
-          id: 2,
-          title: 'Learn Vuex',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'Learn Vue Router',
-          completed: false,
+          todos: [
+            {
+              id: 1,
+              title: 'Learn Vue',
+              completed: false,
+            },
+            {
+              id: 2,
+              title: 'Learn Vuex',
+              completed: false,
+            },
+            {
+              id: 3,
+              title: 'Learn Vue Router',
+              completed: false,
+            },
+          ],
         },
       ],
     };
   },
   methods: {
-    toggleCompleted(i) {
-      this.todos[i].completed = !this.todos[i].completed;
+    toggleCompleted(index) {
+      const { todoIndex, taskIndex } = index;
+      this.lists[todoIndex].todos[taskIndex].completed = !this.lists[todoIndex]
+        .todos[taskIndex].completed;
     },
     addTask(newTask) {
       this.todos.unshift({
