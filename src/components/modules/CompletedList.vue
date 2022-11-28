64cbc45743e5584a84d248889bfd3a7af2702e7f
Shashikant <shashikant.tripathi187@gmail.com>
2022-11-28 14:28:11 +0530

    Application restructed to show as one list.

---
+++ src/components/modules/CompletedList.vue
@@ -0,0 +1,40 @@
+<template>
+  <article class="completed">
+    <h3 class="completed__title">Completed</h3>
+    <ul class="completed__tasks">
+      <template v-for="{ id, title, completed } in list.todos" :key="id">
+        <li v-if="completed" class="completed__task">
+          <the-task :listID="list.id" :id="id" :title="title" :checked="true"></the-task>
+        </li>
+      </template>
+    </ul>
+  </article>
+</template>
+
+<script>
+import TheTask from './TheTask.vue';
+
+export default {
+  name: 'completedList',
+  props: ['list'],
+  components: {
+    TheTask,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.completed {
+  &__tasks {
+    padding-left: 0;
+  }
+
+  &__task {
+    padding-bottom: 0;
+
+    .task__title {
+      text-decoration: line-through;
+    }
+  }
+}
+</style>
---
+++ src/components/modules/TheTask.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="task">
+    <label class="task__label" :for="id">
+      <input
+        :id="id"
+        class="task__checkbox"
+        type="checkbox"
+        :name="title"
+        v-model="completed"
+        @click="toggleCompleted(listID, id)">
+    </label>
+    <p class="task__title">{{ title }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'theTask',
+  props: ['listID', 'id', 'title', 'checked'],
+  inject: ['toggleCompleted'],
+  data() {
+    return {
+      completed: this.checked,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.task {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+
+  padding: 0.5rem 0;
+
+  &__label {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin-right: 0.5rem;
+  }
+
+  &__checkbox {
+    width: 100%;
+    height: 100%;
+  }
+
+  &__title {
+    line-height: normal;
+    padding: 0.1rem 0 0;
+  }
+}
+</style>
