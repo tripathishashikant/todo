5420941899d098d605a03de9aff30627d3779871
Shashikant <shashikant.tripathi187@gmail.com>
2023-04-17 23:59:40 +0530

    Feature 1.6.5 - Theme switcher updated.
    Signed-off-by: Shashikant <shashikant.tripathi187@gmail.com>

--- src/components/global/BaseCard.vue
+++ src/components/global/BaseCard.vue
@@ -9,12 +9,12 @@
   padding: 1.5rem;
   border-radius: 1rem;
   background-color: $secondary-bg-dark;
-  box-shadow: 0 0.1rem 0.2rem 0 rgba($primary-shadow-dark, 1);
+  box-shadow: 0 0.1rem 0.2rem 0 $primary-shadow-dark;
   overflow: hidden;
 
   .light & {
     background-color: $secondary-bg-light;
-    box-shadow: 0 0.1rem 0.2rem 0 rgba($primary-shadow-light, 1);
+    box-shadow: 0 0.1rem 0.2rem 0 $primary-shadow-light;
   }
 }
 </style>
--- src/components/modules/ThemeSwitcher.vue
+++ src/components/modules/ThemeSwitcher.vue
@@ -1,42 +1,47 @@
 <template>
   <div class="themeSwitcher">
-    <p
-      class="themeSwitcher__title"
-      @click="setToggleThemeSwitcher"
+    <div
+      class="themeSwitcher__wrapper"
+      :aria-label="title"
     >
-      {{ title }}
-    </p>
-    <ul
-      v-show="toggleThemeSwitcher"
-      class="themeSwitcher__dropdown"
-    >
-      <li
-        class="themeSwitcher__theme"
-        @click="updateTheme('light')"
-      >
-        Light
-      </li>
-      <li
-        class="themeSwitcher__theme"
+      <div
+        class="themeSwitcher__svg themeSwitcher__svg--moon"
         @click="updateTheme('dark')"
       >
-        Dark
-      </li>
-    </ul>
+        <MoonSVG />
+      </div>
+      <div
+        class="themeSwitcher__svg themeSwitcher__svg--circle"
+        title="circle image"
+      >
+        <span class="visually-hidden">circle</span>
+      </div>
+      <div
+        class="themeSwitcher__svg themeSwitcher__svg--sun"
+        @click="updateTheme('light')"
+      >
+        <SunSVG />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import MoonSVG from '../svgs/MoonSVG.vue';
+import SunSVG from '../svgs/SunSVG.vue';
 
 export default {
   name: 'ThemeSwitcher',
+  components: {
+    SunSVG,
+    MoonSVG,
+  },
   computed: {
     ...mapGetters({
       title: 'themeSwitcherStore/getThemeSwitcherTitle',
       defaultTheme: 'themeSwitcherStore/getDefaultTheme',
       toggleThemeSwitcher: 'themeSwitcherStore/getToggleThemeSwitcher',
-
     }),
   },
   methods: {
@@ -57,7 +62,56 @@ export default {
   position: relative;
   padding: 0 2rem;
 
+  &__wrapper {
+    width: 4.5rem;
+    height: 2.3rem;
+    position: relative;
+    border: none;
+    border-radius: 1.5rem;
+    background-color: $quaternary-bg-dark;
+  }
+
+  &__svg {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 0;
+
+    &--sun {
+      width: 2rem;
+      height: 2rem;
+      left: 0.2rem;
+      cursor: pointer;
+    }
+
+    &--circle {
+      width: 1.7rem;
+      height: 1.7rem;
+      border: 0 none;
+      border-radius: 1.7rem;
+      background-color: $primary-bg-light;
+      z-index: 1;
+      transition: all 0.25s ease-in;
+
+      .dark & {
+        left: calc(100% - 2rem);
+      }
+
+      .light & {
+        left: 0.2rem;
+      }
+    }
+
+    &--moon {
+      width: 1.6rem;
+      height: 2rem;
+      right: 0.3rem;
+      cursor: pointer;
+    }
+  }
+
   &__title {
+    display: none;
     padding: 0;
     cursor: pointer;
   }
