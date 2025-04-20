72481603db0de372ab339493593c74ed06e9aaf1
Shashikant <shashikant.tripathi187@gmail.com>
2025-04-20 19:29:59 +0530

    Site loader added.

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -6,9 +6,9 @@
   >
     <div class="list__cardContainer">
       <base-card
+        class="list__card"
         v-for="{ id, docId, title } in lists"
         :key="id"
-        class="list__card"
       >
         <section class="list__wrapper">
           <div class="list__header">
@@ -18,17 +18,22 @@
             <button
               class="list__deleteBtn nostyle"
               @click="deleteList(docId)"
-            >&#128465;</button>
+            >
+              <DeleteIcon />
+            </button>
           </div>
+
           <div class="list__addTask">
             <add-task :list-doc-id="docId" />
           </div>
+
           <div class="list__pending">
             <pending-list
               :list-doc-id="docId"
               :show-alert-message="false"
             />
           </div>
+
           <div class="list__completed">
             <completed-list
               :list-doc-id="docId"
@@ -38,19 +43,29 @@
           </div>
         </section>
       </base-card>
+
       <div class="list__card list__card--addListBtn">
         <add-list />
       </div>
     </div>
   </div>
+
+  <div
+    class="list__loading"
+    v-else
+  >
+    <LoaderSVG />
+  </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import PendingList from '../components/modules/PendingList.vue';
-import CompletedList from '../components/modules/CompletedList.vue';
-import AddTask from '../components/modules/AddTask.vue';
-import AddList from '../components/modules/AddList.vue';
+import PendingList from '@/components/modules/PendingList.vue';
+import CompletedList from '@/components/modules/CompletedList.vue';
+import AddTask from '@/components/modules/AddTask.vue';
+import AddList from '@/components/modules/AddList.vue';
+import LoaderSVG from '@/components/svgs/LoaderSVG.vue';
+import DeleteIcon from '@/components/svgs/DeleteIcon.vue';
 
 export default {
   name: 'TodoList',
@@ -59,6 +74,8 @@ export default {
     CompletedList,
     AddTask,
     AddList,
+    LoaderSVG,
+    DeleteIcon,
   },
   computed: {
     ...mapGetters({
